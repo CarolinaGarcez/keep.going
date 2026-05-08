29b7ec428e5bc03fff9d17302d516a1a8c5b46da
MARIA CAROLINA GARCEZ ALESSIO <maria.alessio@inss.gov.br>
2026-05-08 15:06:46 -0300

    style: bg mais iluminado e com efeitos novos de cores e parallax

--- js/data/data-status.js
+++ js/data/data-status.js
@@ -2,6 +2,5 @@ export const statusList = [
   "online • respirando • tentando",
   "caos detectado • seguimos",
   "carregando esperança...",
-  "sobrevivendo em modo café",
   "sistema emocional estável"
 ];
